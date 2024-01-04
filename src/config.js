0a9dd45507c9f945c88abd08bd53fcde5c6f88b7
Somu Kandula <74103728+Somu878@users.noreply.github.com>
2024-01-04 19:42:59 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,13 +2,7 @@ import { getAuth } from "firebase/auth";
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDMv1BZTJAeUd5bJ4aY89ayPHVVrvNG_7Q",
-  authDomain: "swiftsignin-6bb10.firebaseapp.com",
-  projectId: "swiftsignin-6bb10",
-  storageBucket: "swiftsignin-6bb10.appspot.com",
-  messagingSenderId: "958276056007",
-  appId: "1:958276056007:web:a807bb115b7e06686f9fe2",
-  measurementId: "G-J6EB88V29C",
+"Add your sdk configuration from firebase here"
 };
 const app = initializeApp(firebaseConfig);
 export const firebaseAuth = getAuth(app);
