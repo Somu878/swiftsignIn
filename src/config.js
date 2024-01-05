66f981cc0290a49cb3099ffd05bf17139f2b4b06
Somu Kandula <74103728+Somu878@users.noreply.github.com>
2024-01-05 21:07:40 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ import { getAuth } from "firebase/auth";
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-"Add your sdk configuration from firebase here"
+"Add your firebase configuration from firebase here"
 };
 const app = initializeApp(firebaseConfig);
 export const firebaseAuth = getAuth(app);
